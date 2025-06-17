623c0941dcf6c550cd05a3323b20d6bef9fa3acb
Prazka <prazkaaldiyuda@gmail.com>
2025-06-17 15:48:44 +0700

    add cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from .routes.predict import predict_route
 from .config import Config
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    CORS(app)
     app.register_blueprint(predict_route)
     return app
